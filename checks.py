0545d9eb9d43cb9dcf4c2d06463bd0e0ca45988a
yair ben zur <benzuryair007@gmail.com>
2026-02-12 10:44:15 +0200

    Implement get_sensitive_port using list comprehension

--- checks.py
+++ checks.py
@@ -3,3 +3,7 @@ from config import *
 
 def get_external_ips(date_list):
     return [i for i in date_list if not i[1].startswith(tuple(INTERNAL_IP))]
+
+
+def get_sensitive_port(date_list):
+    return [i for i in date_list if int(i[3]) in SENSITIVE_PORT]
