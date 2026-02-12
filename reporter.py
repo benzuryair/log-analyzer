42734ae700d446ab73afb50dfb2c2ccf56423ff8
yair ben zur <benzuryair007@gmail.com>
2026-02-12 16:28:27 +0200

    Add get_suspicions function to identify potential threats

--- reporter.py
+++ reporter.py
@@ -1,3 +1,6 @@
+from config import *
+
+
 def get_source_ip(date):
     new = [j[1] for j in date]
     return {i: new.count(i) for i in new}
@@ -5,3 +8,28 @@ def get_source_ip(date):
 
 def get_protocol(date):
     return {int(i[3]): i[4] for i in date}
+
+
+def get_suspicions(date):
+    suspicions_dikt = {}
+    for i in date:
+        suspicions_list = []
+        if not i[1].startswith(tuple(INTERNAL_IP)):
+            suspicions_list.append("external_ips")
+
+        if int(i[3]) in SENSITIVE_PORT:
+            suspicions_list.append("sensitive_port")
+
+        if int(i[5]) > LARGE_PACKET:
+            suspicions_list.append("large_packet")
+
+        if NIGHT_START <= int(i[0][11:13]) < NIGHT_END:
+            suspicions_list.append("night_activity")
+
+        if i[1] in suspicions_dikt:
+            for j in suspicions_dikt[i[1]]:
+                if j not in suspicions_list:
+                    suspicions_list.append(j)
+        suspicions_dikt[i[1]] = suspicions_list
+
+    return suspicions_dikt
