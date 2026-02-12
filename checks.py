4abe3493096b8a4eea27c749b7ad96ed2f28e9a8
yair ben zur <benzuryair007@gmail.com>
2026-02-12 10:54:38 +0200

    Implement tag_traffic classification using list comprehension

--- checks.py
+++ checks.py
@@ -11,3 +11,7 @@ def get_sensitive_port(date_list):
 
 def get_large_packet(date_list):
     return [i for i in date_list if int(i[5]) > LARGE_PACKET]
+
+
+def tag_traffic(date_list):
+    return ["LARGE" if int(i[5]) > LARGE_PACKET else "NORMAL" for i in date_list]
