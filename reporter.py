d9bc094975c492f76be20742d26c824f3700dd2d
yair ben zur <benzuryair007@gmail.com>
2026-02-12 14:27:55 +0200

    Implement port-to-protocol mapping in reporter.py

--- reporter.py
+++ reporter.py
@@ -1,3 +1,7 @@
 def get_source_ip(date):
-    new=[j[1] for j in date]
+    new = [j[1] for j in date]
     return {i: new.count(i) for i in new}
+
+
+def get_protocol(date):
+    return {int(i[3]): i[4] for i in date}
